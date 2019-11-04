a90a4097b60405419ea35fa588879a2d2ead5f84
teknofage <teknofage@gmail.com>
2019-11-04 14:41:57 -0800

    results page working

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -8,10 +8,10 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/fortune')
+@app.route('/fortune/new')
 def fortune():
     """Renders the home Fortune page."""
-    return render_template('fortune_results.html')
+    return render_template('fortune_form.html')
 
 
 @app.route('/weather')
@@ -19,24 +19,37 @@ def weather():
     """Renders the Weather page."""
     return render_template('weather_results.html')
 
+# @app.route('/fortunes', methods=['POST'])
+# def fortunes_submit():
+#     """Submit a new fortune."""
+#     fortune = {
+#         'title': request.form.get('title'),
+#         'description': request.form.get('description'),
+#         'videos': request.form.get('videos').split(),
+#         'ratings': request.form.get('ratings')
+#     }
+#     playlist_id = playlists.insert_one(playlist).inserted_id
+#     return redirect(url_for('playlists_show', playlist_id=playlist_id))
 
 @app.route('/fortune_results')
 def fortune_results():
     """Displays the user's fortune."""
     users_preferred_drink = request.args.get('drink')
-    # ... etc
+    title = request.args.get('title')
+    name = request.args.get('firstname')
 
     if users_preferred_drink == 'Beer':
-        fortune is "You'll grow a beer belly and stop shaving as often!"
+        fortune = "You'll grow a beer belly and stop shaving as often!"
     elif users_preferred_drink == 'Wine':
-        fortune is "You'll tell the best stories but your head will hut in the mornings!"
+        fortune = "You'll tell the best stories but your head will hurt in the mornings!"
     elif users_preferred_drink == 'Mead':
-        fortune is "You'll braid your hair and wield an axe with competence!"
+        fortune = "You'll braid your hair and wield an axe with competence!"
     elif users_preferred_drink == 'Elderflower Cordial':
-        fortune is "Your senses will never be dull, unlike your speeches!"
+        fortune = "Your senses will never be dull, unlike your speeches!"
     elif users_preferred_drink == 'Love Potion Number 9':
-        fortune is "You will fall madly in love with the wrong person, but it will be awesome while it lasts!"
+        fortune = "You will fall madly in love with the wrong person, but it will be awesome while it lasts!"
     elif users_preferred_drink == 'Gorilla Punch':
-        fortune is "You will fall over and need to be dragged to bed. Try not to wear your armour or you won't be able to get up!"
-    else
-        fortune is "You will feel unusually good about your quest for the Grail, even if you never find it!"
+        fortune = "You will fall over and need to be dragged to bed. Try not to wear your armour or you won't be able to get up!"
+    else:
+        fortune = "You will feel unusually good about your quest for the Grail, even if you never find it!"
+    return render_template('fortune_results.html', fortune=fortune, title=title, name=name)
